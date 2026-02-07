97cd3fc1bd327c09b413f093473bd523f76eeefa
Yazanleva <yazannalsalloumi@gmail.com>
2026-02-07 12:12:45 +0100

    TASK1: S1_VAR_01 - catalog values and types

--- stage1/S1_VAR_01.py
+++ stage1/S1_VAR_01.py
@@ -1,1 +1,15 @@
+values = {
+    "int": 1,
+    "float": 1.5,
+    "str": "hi",
+    "bool": True,
+    "none": None,
+    "list": [1, 2],
+    "tuple": (1, 2),
+    "dict": {"a": 1},
+    "set": {1, 2},
+    "function": lambda x: x
+}
 
+for name, val in values.items():
+    print(name, val, type(val), type(val).__name__)
