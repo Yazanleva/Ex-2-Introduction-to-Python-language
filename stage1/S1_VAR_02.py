2d2a7ec955dc7b794deeafb23fa89eb5681dc607
Yazanleva <yazannalsalloumi@gmail.com>
2026-02-07 12:13:28 +0100

    TASK1: S1_VAR_02 - demonstrate dynamic typing

---
+++ stage1/S1_VAR_02.py
@@ -0,0 +1,13 @@
+x = 1
+print(x, type(x).__name__)
+
+x = "hi"
+print(x, type(x).__name__)
+
+x = [1, 2]
+print(x, type(x).__name__)
+
+x = None
+print(x, type(x).__name__)
+
+# Dynamic typing means a variable can be rebound to values of different types at runtime.
